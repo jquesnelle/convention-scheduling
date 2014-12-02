a28003c88bf53258f04c86bb0f111fa23c6f172d
Jeffrey Quesnelle <jquesnelle@gmail.com>
2014-12-02 15:52:19 -0500

    better pco script for dan

--- code/execute_run_pco_once.sh
+++ code/execute_run_pco_once.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Generating database"
 python main.py --generate-db schedule2013.html
-echo "Setting up database with at most $1 talks/$2 hours, and $3 attendees with $4 rsvps each"
-python main.py --setup-db-2013 schedule2013.html.db $1 $2 $3 $4
+echo "Setting up database with at most $1 talks/$2 hours, and $3 attendees with $4 rsvps each and sigma ratio $5"
+python main.py --setup-db-2013normalclass schedule2013.html.db $1 $2 $3 $4 $5
 echo "Generating model"
-python main.py --generate-model-pco schedule2013.html.db
+python main.py --generate-model-pcodualize schedule2013.html.db
 echo "Solving model"
 cbc schedule2013.html.db.lp -threads 6 -solve -solu output.sol 
 echo "Importing solution"
